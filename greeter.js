67f5cdd23f32528f45a984b58ab6914a2c32c476
Wayne Lawler <wlawler4145@gmail.com>
2017-01-31 20:06:54 -0600

    Add Java file

---
+++ greeter.js
@@ -0,0 +1,24 @@
+
+
+var name = prompt('Are you looking for a graphic designer?');
+  if (name === 'yes') alert('Congrats! You are about to start a magical journey into the world of design');
+  else alert('Well, I\'m also somewhat friendly adn a good listener')
+
+console.log('Hello World');
+
+var carColor = prompt('What color is your car?');
+
+var myColor = function() {
+  if(carColor === 'red') alert('You are a hot rod');
+    else if(carColor === 'blue') alert('You\'re pretty COOL');
+    else if(carColor === 'green') alert('No thanks hippie');
+    else if(carColor === 'white') alert('Wash it');
+    else if(carColor === 'black') alert('Did we just become best friends?');
+    else if(carColor === 'maroon') alert('Is it also a Cadillac? Cuz you old...');
+    else if(carColor === 'silver') alert('Awesome');
+  else alert('You suck as a person');
+};
+
+
+
+myColor();
